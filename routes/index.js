692c8e4d26152c377cb87910787bfe2e6f36dac9
Twenmod <Twengames@gmail.com>
2023-07-01 20:07:51 +0200

    init

---
+++ routes/index.js
@@ -0,0 +1,110 @@
+var express = require('express');
+const path = require('path');
+var router = express.Router();
+
+//Get spreadsheets
+const fs = require('fs').promises;
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+const { json } = require('express');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+var data = [{amount:-1}];
+var amountA = 0
+var amountB = 0
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+async function listMajors(auth) {
+  const sheets = google.sheets({version: 'v4', auth});
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: '1sLR8PmU_g1KoLnQiPGGzoG7WdytM4it8rES2JgxTIns',
+    range: 'Data!B2:B3',
+  });
+  const rows = res.data.values;
+
+  amountA = rows[0][0];
+  amountB = rows[1][0];
+
+  console.log("A: "+ amountA + "\nB: "+amountB);
+
+}
+
+
+
+authorize().then(listMajors).catch(console.error);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  authorize().then(listMajors).catch(console.error);
+  console.log("gtest " + data);
+  if (amountA > amountB) {
+    res.render('B.pug');
+  }else {
+    res.render('A.pug');
+  }
+});
+
+module.exports = router;
