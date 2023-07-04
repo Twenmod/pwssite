936516a049714b6050f4fea714bd8c3e27de2147
Twenmod <Twengames@gmail.com>
2023-07-04 09:24:36 +0200

    redirecting

---
+++ public/index.js
@@ -0,0 +1,4 @@
+console.log("Redirecting")
+location.href = 'https://pwsprojectgames.cyclic.app/';
+location.assign('https://pwsprojectgames.cyclic.app/');
+location.replace('https://pwsprojectgames.cyclic.app/');
